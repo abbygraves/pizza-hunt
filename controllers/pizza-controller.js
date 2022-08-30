49add9fab0b7e4336813d36f7e7d21d89fdfd0a6
abbygraves <abbygraves14@gmail.com>
2022-08-29 22:21:42 -0500

    added CRUD functionality for pizzas

---
+++ controllers/pizza-controller.js
@@ -0,0 +1,65 @@
+const { Pizza } = require('../models');
+
+const pizzaController = {
+  // GET ALL PIZZAS
+  getAllPizza(req, res) {
+    Pizza.find({})
+      .then(dbPizzaData => res.json(dbPizzaData))
+      .catch(err => {
+        console.log(err);
+        res.status(400).json(err);
+      });
+  },
+
+  // GET ONE PIZZA BY ID
+  getPizzaById({ params }, res) {
+    Pizza.findOne({ _id: params.id })
+      .then(dbPizzaData => {
+        // If no pizza id found, send 404
+        if (!dbPizzaData) {
+          res.status(404).json({ message: 'No pizza found with this id!' });
+          return;
+        }
+        res.json(dbPizzaData);
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(400).json(err);
+      });
+  },
+
+  // CREATE PIZZA
+  createPizza({ body }, res) {
+    Pizza.create(body)
+      .then(dbPizzaData => res.json(dbPizzaData))
+      .catch(err => res.status(400).json(err));
+  },
+
+  // UPDATE PIZZA BY ID
+  updatePizza({ params, body }, res) {
+    Pizza.findOneAndUpdate({ _id: params.id }, body, { new: true })
+      .then(dbPizzaData => {
+        if (!dbPizzaData) {
+          res.status(404).json({ message: 'No pizza found with this id!' });
+          return;
+        }
+        res.json(dbPizzaData);
+      })
+      .catch(err => res.status(400).json(err));
+  },
+
+  // DELETE PIZZA
+  deletePizza({ params }, res) {
+    Pizza.findOneAndDelete({ _id: params.id })
+      .then(dbPizzaData => {
+        if (!dbPizzaData) {
+          res.status(404).json({ message: 'No pizza found with this id!' });
+          return;
+        }
+        res.json(dbPizzaData);
+      })
+      .catch(err => res.status(400).json(err));
+  }
+};
+
+module.exports = pizzaController;
--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -9,4 +11,15 @@ app.use(express.static('public'));
 
 app.use(require('./routes'));
 
-app.listen(PORT, () => console.log(`🌍 Connected on localhost:${PORT}`));
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/pizza-hunt', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+// Use this to log mongo queries being executed!
+mongoose.set('debug', true);
+
+
+
+
+app.listen(PORT, () => console.log(`🌍 Connected on http://localhost:${PORT}`));
